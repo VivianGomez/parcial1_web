d828c1a1a7b54b02f03497e774d2024e292ba850
vngomez <vn.gomez@uniandes.edu.co>
2018-09-19 16:53:53 -0500

    prueba heroku

--- routes/parcial1.js
+++ routes/parcial1.js
@@ -1,1 +1,11 @@
-parcial1
+"use strict";
+const config = require("../env.js");
+
+const MongoClient = require('mongodb').MongoClient;
+
+module.exports = router => {
+
+// Metodos CRUD, para el API
+
+  return router;
+};
