5245089dab08c896f2faf5849537da511aedd22c
vngomez <vn.gomez@uniandes.edu.co>
2018-09-19 04:10:00 -0500

    configurations

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # parcial1_web
+
+Para correr la app
+
+Para correr el back, específicamente express, ejecutar en la consola:
+
+`
+nodemon
+`
+
+este es un módulo que permite ver los cambios realizados en el back sin tener que volver a ejecutar el archivo index.js
--- index.js
+++ index.js
@@ -1,14 +1,68 @@
-// javascript en modo estricto
+// Se usa javascript en modo estricto
 "use strict";
 
-//Express framework para ejecutar el Back-End con Node.js
-var express = require('express');
-var app = express();
+// Archivo que permite configurar ciertos elementos a partir del ambiente en el que se corre el proyecto
+//const env = require('./env');
 
-app.get('/', function (req, res) {
-  res.send('Hello World! EXPRESSSSSSSSSS!!!!');
+// Se usa Express, una infraestructura web rápida, minimalista y flexible para Node.js
+const express = require("express");
+
+// Inicializacion de Express
+const app = express();
+
+// Objeto para las rutas - API
+const router = express.Router();
+
+
+// Se usa Body-Parser, se encarga de analizar los cuerpos (body) de las solicitudes en el middleware antes que nuestros manejadores lo hagan
+const bodyParser = require("body-parser");
+
+// Se usa CORS, que permite proveer un middleware Connect/Express que puede ser usado para habilitar CORS con varias opciones
+//const cors = require("cors");
+
+// Se usa para poder implementar paths
+const path = require('path');
+
+// Puerto a usar. Se tiene en cuenta el caso de Heroku
+const port = process.env.PORT || 3001;
+
+// Se obtiene la configuración de la base de datos
+//const config = require("./database");
+
+// Ruta api
+const rutaApi = '/parcial1/api/';
+
+// Configura las peticiones con cuerpo de tipo JSON
+app.use(
+    bodyParser.json({
+        limit: "1mb"
+    })
+);
+
+// Configura las peticiones con cuerpo de tipo url encoded
+app.use(
+    bodyParser.urlencoded({
+        limit: "1mb",
+        extended: true
+    })
+);
+
+
+// Se define la ruta de archivos estáticos para el front
+app.use(express.static(path.resolve(__dirname, 'client/build')));
+
+
+//Se conecta al front
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+//Se define el puerto en el que trabaja el servidor
+app.listen(port, () => {
+    console.log(
+        "El servidor de la aplicación está corriendo en el puerto " +
+        port +
+        " en modo " +
+        process.env.NODE_ENV
+    );
 });
