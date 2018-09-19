e1d23e1d42ccd10e76a8f1040dfe8a6c405c39b2
vngomez <vn.gomez@uniandes.edu.co>
2018-09-19 09:50:58 -0500

    db y otras configuraciones

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# parcial1_web
+# Parcial 1 dev_web
 
-Para correr la app
+### Para correr la app
 
 Para correr el back, específicamente express, ejecutar en la consola:
 
@@ -8,4 +8,12 @@ Para correr el back, específicamente express, ejecutar en la consola:
 nodemon
 `
 
-este es un módulo que permite ver los cambios realizados en el back sin tener que volver a ejecutar el archivo index.js
+Este es un módulo que permite ver los cambios realizados en el back sin tener que volver a ejecutar el archivo index.js
+
+Para correr el front,  ejecutar en otra consola:
+
+`
+npm start
+`
+
+O simplemente en https://web-dev-exam1.herokuapp.com/, si no se quiere descargar nada y sólo se quiere ver la app.
--- index.js
+++ index.js
@@ -1,45 +1,40 @@
-// Se usa javascript en modo estricto
+// Strict JavaScript mode is used
 "use strict";
 
-// Archivo que permite configurar ciertos elementos a partir del ambiente en el que se corre el proyecto
-//const env = require('./env');
+// File that allow to configure aspects of the application
+const env = require('./env');
 
-// Se usa Express, una infraestructura web rápida, minimalista y flexible para Node.js
+// Fast, unopinionated, minimalist web framework for node.
 const express = require("express");
 
-// Inicializacion de Express
+// Express initialization
 const app = express();
 
-// Objeto para las rutas - API
+// Object for the routes
 const router = express.Router();
 
-
-// Se usa Body-Parser, se encarga de analizar los cuerpos (body) de las solicitudes en el middleware antes que nuestros manejadores lo hagan
+// Node.js body parsing middleware.
 const bodyParser = require("body-parser");
 
-// Se usa CORS, que permite proveer un middleware Connect/Express que puede ser usado para habilitar CORS con varias opciones
-//const cors = require("cors");
-
-// Se usa para poder implementar paths
+// Used for allowing paths
 const path = require('path');
 
-// Puerto a usar. Se tiene en cuenta el caso de Heroku
+// Port to use
 const port = process.env.PORT || 3001;
 
-// Se obtiene la configuración de la base de datos
-//const config = require("./database");
+// API route
+const apiRoute = "/parcial1/";
 
-// Ruta api
-const rutaApi = '/parcial1/api/';
 
-// Configura las peticiones con cuerpo de tipo JSON
+
+// JSON body request is configured
 app.use(
     bodyParser.json({
         limit: "1mb"
     })
 );
 
-// Configura las peticiones con cuerpo de tipo url encoded
+// Url encoded body request is configured
 app.use(
     bodyParser.urlencoded({
         limit: "1mb",
@@ -47,22 +42,19 @@ app.use(
     })
 );
 
-
-// Se define la ruta de archivos estáticos para el front
+// Route of static files is defined
 app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 
-//Se conecta al front
-app.get('*', function(request, response) {
+// Connection to front
+app.get('*', function (request, response) {
     response.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
-//Se define el puerto en el que trabaja el servidor
+// Port is defined and app is connected
 app.listen(port, () => {
     console.log(
-        "El servidor de la aplicación está corriendo en el puerto " +
-        port +
-        " en modo " +
-        process.env.NODE_ENV
+        "The application server is running on the port " +
+        port + " in " + process.env.NODE_ENV + " mode"
     );
 });
